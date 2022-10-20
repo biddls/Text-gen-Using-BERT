124a190da1e048b1bdb6db40cc68c660c7386085
biddls <biddls69420@gmail.com>
2022-10-20 17:26:52 +0100

    video downloading sorted

---
+++ Downloader.py
@@ -0,0 +1,65 @@
+import concurrent.futures
+import os
+from pytube import YouTube
+from pytube import Channel
+from typing import Union, Callable, Pattern
+import whisper
+
+
+# tesing
+from time import time
+
+
+def downloadVideoAudio(_videoLink: str, _regex: Union[Pattern, Callable[[str], str]], _channelName: str, _count=0):
+    # checks if it's been downloaded before
+    for vid in os.listdir(_channelName):
+        if _videoLink.split("=")[-1] in vid:
+            return
+
+    # queues up the file to be downloaded
+    yt = YouTube(_videoLink)
+    audio_file = yt.streams.filter(only_audio=True, file_extension='mp4').order_by('abr').asc().first()
+
+    # makes certain checks
+    if audio_file.default_filename == 'Video Not Available.mp4':
+        if _count > 10:
+            print(f'Download failed {_videoLink=}')
+            return
+        downloadVideoAudio(_videoLink, _regex, _channelName, _count + 1)
+        return
+    if isinstance(_regex, Pattern):
+        if (a := _regex.search(audio_file.default_filename)) is None:
+            print(f'regex output: {a}\n {audio_file.default_filename=} {_videoLink=} {_count=}')
+            return
+        a = a.string[a.regs[1][0]:a.regs[1][1]]
+    else:
+        if (a := _regex(audio_file.default_filename)) is False:
+            print(f'function output: {a}\n {audio_file.default_filename=}')
+            return
+    # downloads the file
+    _videoLink = f"{_videoLink.split('=')[-1]}_{a.replace(' ', '_')}"
+    if os.path.exists(f'{_channelName}/{_videoLink}.mp4'):
+        return
+    print(f'Downloading: {_videoLink=}')
+    audio_file.download(output_path=_channelName, filename=f'{_videoLink}.mp4')
+
+
+def downloadChannelAudio(_channel: str, _regex: Union[Pattern, Callable[[str], str]], _count: int, _threads: int):
+    _count = -1 if _count < 1 else _count
+
+    if not os.path.exists(_channel):
+        os.makedirs(_channel)
+    s = time()
+    if _threads > 1:
+        with concurrent.futures.ProcessPoolExecutor(max_workers=_threads) as executor:
+            for video in Channel(f"https://www.youtube.com/c/{_channel}/videos")[:_count]:
+                executor.submit(downloadVideoAudio, video, _regex, _channel)
+    else:
+        for video in Channel(f"https://www.youtube.com/c/{_channel}/videos")[:_count]:
+            downloadVideoAudio(video, _regex, _channel)
+    print(f"it took {time() - s} seconds to downloads")
+
+    model = whisper.load_model("base")
+    print("HAI")
+    result = model.transcribe("TheDailyGwei\\8wVaBFvUxyg_The_Daily_Gwei_Refuel_471.mp4")
+    print(result["text"])
--- test.py
+++ test.py
@@ -1,31 +1,7 @@
-from pytube import YouTube
-from pytube import Channel
-from tqdm import tqdm
-import os
-from time import sleep
+import Downloader
+import re
 
 
 if __name__ == "__main__":
-    if not os.path.exists('Daily_Gwei'):
-        os.makedirs('Daily_Gwei')
-    os.chdir("Daily_Gwei")
-    if not os.getcwd().endswith("Daily_Gwei"):
-        raise FileNotFoundError("Not in corrent dir")
-
-    for video in Channel("https://www.youtube.com/c/TheDailyGwei/videos"):
-        yt = YouTube(video)
-        audio_file = yt.streams.filter(only_audio=True).order_by('abr').desc().first()
-        print(audio_file.default_filename, video)
-    exit(0)
-
-    channel = tqdm(Channel("https://www.youtube.com/c/TheDailyGwei/videos"))
-    for video in channel:
-        # print(video)
-        yt = YouTube(video)
-        audio_file = yt.streams.filter(only_audio=True).order_by('abr').desc().first()
-        # print(audio_file)
-        channel.set_description(audio_file.default_filename.split(" - ")[-2])
-        sleep(0.5)
-        # exit(0)
-        # audio_file.download()
-
+    test = re.compile("(The Daily Gwei Refuel \d+)?=* - Ethereum Updates.mp4")
+    Downloader.downloadChannelAudio("TheDailyGwei", test, 10, 10)
