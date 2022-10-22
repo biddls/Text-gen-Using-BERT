16e600d2fd4d1cb642b9348a430035a0842ca1c0
biddls <biddls69420@gmail.com>
2022-10-22 23:28:37 +0100

    full auto system working, need to work on tidying the data structure

--- Downloader.py
+++ Downloader.py
@@ -5,57 +5,64 @@ from pytube import Channel
 from typing import Union, Callable, Pattern
 import whisper
 from whisper import Whisper
+from tqdm import tqdm
 
-def downloadVideoAudio(_videoLink: str, _regex: Union[Pattern, Callable[[str], str]], _channelName: str, _count=0):
-    # queues up the file to be downloaded
-    yt = YouTube(_videoLink)
+
+def downloadVideoAudio(
+        _fileName: str,
+        _regex: Union[Pattern, Callable[[str], str]],
+        _channelName: str,
+        _count=0):
+
+    yt = YouTube(_fileName)
     audio_file = yt.streams.filter(only_audio=True, file_extension='mp4').order_by('abr').asc().first()
 
     # makes certain checks
     if audio_file.default_filename == 'Video Not Available.mp4':
         if _count > 10:
-            print(f'Download failed {_videoLink=}')
+            print(f'Download failed {_fileName=}')
             return
-        downloadVideoAudio(_videoLink, _regex, _channelName, _count + 1)
+        downloadVideoAudio(_fileName, _regex, _channelName, _count + 1)
         return
     if isinstance(_regex, Pattern):
         if (a := _regex.search(audio_file.default_filename)) is None:
-            print(f'regex output: {a}\n {audio_file.default_filename=} {_videoLink=} {_count=}')
+            print(f'regex output: {a}\n {audio_file.default_filename=} {_fileName=} {_count=}')
             return
         a = a.string[a.regs[1][0]:a.regs[1][1]]
     else:
         if (a := _regex(audio_file.default_filename)) is False:
             print(f'function output: {a}\n {audio_file.default_filename=}')
             return
-    # downloads the file
-    _videoLink = f"{_videoLink.split('=')[-1]}_{a.replace(' ', '_')}"
-    if os.path.exists(f'{_channelName}/{_videoLink}.mp4'):
-        return
-    print(f'Downloading: {_videoLink=}')
-    audio_file.download(output_path=_channelName, filename=f'{_videoLink}.mp4')
+
+    _fileName = f"{_fileName.split('=')[-1]}_{a.replace(' ', '_')}.mp4"
+
+    # if the video has already been downloaded skip it
+    if not os.path.exists(f'{_channelName}/{_fileName}'):
+        print(f'Downloading: {_fileName=}')
+        audio_file.download(output_path=_channelName, filename=_fileName)
 
 
-def runModel(_model: Whisper, _file: str) -> str:
+def runModel(
+        _model: Whisper,
+        _file: str) -> str:
     result = _model.transcribe(_file, language="en")["text"]
+
+    # saves output of the model to a text file
     with open(f'{_file.replace(".mp4", ".txt")}', 'w') as f:
         f.write(result)
 
-    # os.rename(_file, _file.split("/")[0]+"/processed_"+_file.split("/")[1])
+    # adds tag to audio file that it's been processed
+    os.rename(_file, _file.replace("/", "/_"))
     return result
 
 
-def transcribeAudio(_model: str, _dir: str) -> [str]:
-    if _model not in whisper.available_models():
-        raise ValueError(str(f"{_model} has to be in {whisper.available_models()}"))
+def transcribeDirAudio(_model: str, _dir: str) -> [str]:
     model = whisper.load_model(_model)
     results = list()
-    for file in os.listdir(_dir):
-        # checks if it's been processes before
-        if file.startswith("processed_"):
-            continue
-        # only process mp4 files
-        if file.endswith(".mp4"):
-            results.append(runModel(model, f"{_dir}/{file}",))
+
+    files = [file for file in os.listdir(_dir) if (not file.startswith("_") and file.endswith(".mp4"))]
+    for _file in tqdm(files, desc=f"Speach to text inf using {_model}"):
+        results.append(runModel(model, f"{_dir}/{_file}",))
 
     return results
 
@@ -70,7 +77,6 @@ def downloadChannelAudio(
 
     if not os.path.exists(_channel):
         os.makedirs(_channel)
-
     with concurrent.futures.ProcessPoolExecutor(max_workers=_threads) as executor:
         for video in Channel(f"https://www.youtube.com/c/{_channel}/videos")[:_count]:
             # checks if it's been downloaded before
--- test.py
+++ test.py
@@ -1,11 +1,7 @@
-import time
-import Downloader
+from Downloader import downloadChannelAudio, transcribeDirAudio
 import re
 
 if __name__ == "__main__":
     test = re.compile('(The Daily Gwei Refuel \d+)?=* - Ethereum Updates.mp4')
-    Downloader.downloadChannelAudio('TheDailyGwei', test, 10, 10)
-    # results = Downloader.transcribeAudio('medium.en', 'TheDailyGwei')
-    results = Downloader.transcribeAudio('tiny.en', 'TheDailyGwei')
-    for result in results:
-        print(result)
+    downloadChannelAudio('TheDailyGwei', test, 10, 2)
+    results = transcribeDirAudio('medium.en', 'TheDailyGwei')
